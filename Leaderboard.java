3ee1d508c364d9d0a4593724ba828643a18d2187
hyungjikim26 <143664962+hyungjikim26@users.noreply.github.com>
2024-11-25 16:32:15 -0700

    Implement load and save method for Leaderboard

--- Leaderboard.java
+++ Leaderboard.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -6,7 +11,7 @@ public class Leaderboard {
     private static final String FILENAME = "leaderboard.txt";
     // return only the top 10 scores
     private static final int MAX_ENTRIES = 10;
-    private ArrayList<Entry> entries;
+    private final ArrayList<Entry> entries;
 
     public Leaderboard() {
         entries = new ArrayList<>();
@@ -21,13 +26,37 @@ public class Leaderboard {
         save();
     }
 
+    /**
+     * 
+     * @pre leaderboard.txt either doesn't exist or, it exists, is readable, and
+     * has the correct format
+     * @post entries is populated with the scores from the file
+     */
     private void load() {
-
+        // read from the external file leaderboard.txt, populate the entry
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILENAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] input = line.split(";");
+                entries.add(new Entry(input[0], Integer.parseInt(input[1])));
+            }
+        } catch (IOException e) {
+            // if file doesn't exist already, create new empty entry file
+            save();
+        }
     }
 
     private void save() {
-
+        // write to the external file leaderboard.txt
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME))) {
+            for (Entry entry : entries) {
+                writer.write(entry.getName() + ";" + entry.getScore() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error saving leaderboard: " + e.getMessage());
+        }
     }
+
 
     public ArrayList<Entry> getTopScore() {
         // return only the top 10 scores
@@ -38,6 +67,4 @@ public class Leaderboard {
         // return unmodifiable list
         return new ArrayList<>(entries);
     }
-
-
 }

125367246d587270cabca66614dcda012afc1abc
JwonLe <juwonlee@arizona.edu>
2024-12-05 10:12:03 -0700

    add score effect

--- TimeListener.java
+++ TimeListener.java
@@ -2,8 +2,8 @@
  * File: TimeListener.java
  * Authors: Claire O'Brien (obrien9), Hyungji Kim (hyungjikim),
  *          Juwon Lee (juwonlee), Tatiana Rastoskueva (trastoskueva)
- * Purpose:
- * 
+ * Purpose: provide a callback mechanism  for handling time-based events. 
+ * 			It allows objects to receive updates about elapsed time.
  */
 
 public interface TimeListener {

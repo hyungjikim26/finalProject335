67c868d05f5f8ba1e007dc18c0b6f534efdf45bc
Claire <claireobrien056@gmail.com>
2024-12-06 21:55:21 -0700

    save changes

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,6 @@
  * Purpose:
  * 
  */
-
 import java.util.ArrayList;
 import java.util.Random;
 
--- src/BoardGUI.java
+++ src/BoardGUI.java
@@ -6,7 +6,6 @@
  *          displaying tiles and handling user interactions.
  * 
  */
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;

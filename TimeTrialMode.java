97ab4fd918cca9e7dcff5f6da70e548e5607789e
JwonLe <juwonlee@arizona.edu>
2024-12-06 18:08:09 -0700

    add comments

--- TimeTrialMode.java
+++ TimeTrialMode.java
@@ -13,18 +13,32 @@ public class TimeTrialMode extends Board {
     private final Timer timer;
     private boolean timeUp;
 
+    /**
+     * Default constructor for TimeTrialMode.
+     */
     public TimeTrialMode() {
     	super();
         timeUp = false;
         timer = new Timer();
     }
      
+    /**
+     * Constructor for TimeTrialMode with specified board size and number of tiles.
+     * @param size     the size of the board
+     * @param numTiles the initial number of tiles
+     */
     public TimeTrialMode(int size, int numTiles) {
         super(size, numTiles);
         timeUp = false;
         timer = new Timer();
     }
 
+    /**
+     * Starts the timer for the Time Trial mode.
+     * The timer updates the remaining time every second and notifies the listener.
+     * When time runs out or the game is over, the timer is stopped.
+     * @param listener a TimeListener object to be notified of time updates
+     */
     public void start(TimeListener listener){
         TimerTask task = new TimerTask() {
             long remaining = TIME_LIMIT;
@@ -74,6 +88,10 @@ public class TimeTrialMode extends Board {
 
     }
 
+    /**
+     * Checks whether the time limit has been reached.
+     * @return true if the time is up, false otherwise
+     */
     public boolean getTimeUp(){
         return timeUp;
     }
